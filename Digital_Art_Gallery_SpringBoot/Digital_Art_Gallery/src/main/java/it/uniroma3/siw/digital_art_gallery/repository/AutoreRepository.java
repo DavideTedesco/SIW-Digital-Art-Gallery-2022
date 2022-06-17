1b5c9279eead4a4d7e6d11ead90adbd8566c3a05
Skesky <kevin03023@gmail.com>
2022-06-18 01:39:49 +0200

    update

--- Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/repository/AutoreRepository.java
+++ Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/repository/AutoreRepository.java
@@ -6,4 +6,6 @@ import it.uniroma3.siw.digital_art_gallery.model.Autore;
 
 public interface AutoreRepository extends CrudRepository<Autore, Long> {
 
+		boolean existsByNomeAndCognomeAndDataDiNascita(String nome, String Cognome, 
+				java.time.LocalDate dataDiNascita);
 }
--- Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/validator/AutoreValidator.java
+++ Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/validator/AutoreValidator.java
@@ -1,19 +1,47 @@
 package it.uniroma3.siw.digital_art_gallery.validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import it.uniroma3.siw.digital_art_gallery.model.Autore;
+import it.uniroma3.siw.digital_art_gallery.service.AutoreService;
+
 public class AutoreValidator implements Validator {
+	
+	 final Integer MAX_NOME_LENGTH = 100;
+	 final Integer MAX_COGNOME_LENGTH = 100;
+	 final Integer MIN_NOME_LENGTH = 100;
+	 final Integer MIN_COGNOME_LENGTH = 100;
+	 
+	 @Autowired
+	 AutoreService autoreService;
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
-		return false;
+		return Autore.class.equals(clazz);
 	}
 
 	@Override
-	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+	public void validate(Object o, Errors errors) {
+		 Autore autore = (Autore) o;
+	        String nome = autore.getNome().trim();
+	        String cognome = autore.getCognome().trim();
+	        java.time.LocalDate dataDiNascita = autore.getDataDiNascita();
+
+	        if (nome.isEmpty())
+	            errors.rejectValue("nome", "required");
+	        else if (nome.length() < MIN_NOME_LENGTH || nome.length() > MAX_NOME_LENGTH)
+	            errors.rejectValue("nome", "size");
+
+	        if (cognome.isEmpty())
+	            errors.rejectValue("cognome", "required");
+	        else if (cognome.length() < MIN_COGNOME_LENGTH || cognome.length() > MAX_COGNOME_LENGTH)
+	            errors.rejectValue("cognome", "size");
+	        
+	        if(this.autoreService.verificaDuplicati(nome, cognome, dataDiNascita)) {
+	        	errors.reject("duplicati");
+	        }
 
 	}
 
