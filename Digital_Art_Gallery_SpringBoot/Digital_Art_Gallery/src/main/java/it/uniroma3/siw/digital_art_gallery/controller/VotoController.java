2fbf079b70fb06156b889b59fd2658aa82a43e7a
Davide Tedesco <davide_hrd@hotmail.it>
2022-06-25 01:02:05 +0200

    modified grade stuff

--- Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/controller/VotoController.java
+++ Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/controller/VotoController.java
@@ -1,5 +1,7 @@
 package it.uniroma3.siw.digital_art_gallery.controller;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -38,21 +40,25 @@ public class VotoController {
 		return "effettuaVotazione";
 	}
 	
+	@Transactional
 	@PostMapping("/votazione/{id}")
 	public String effettuandoVotazione(@PathVariable("id") Long id,
-										@ModelAttribute("voto") Voto voto,Model model) {
+										@ModelAttribute("voto") Voto voto,
+										Model model) {
 		Opera opera = this.operaService.findOperaById(id);
-		opera.getVoti().add(voto);
+//		opera.getVoti().add(voto);
 		voto.setOpera(opera);
+		
 		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
 		User user = credentials.getUser();
 		voto.setUser(user);
-		this.votoService.save(voto);
 		
-		model.addAttribute("artwork", voto.getOpera());
+		this.votoService.save(voto, opera);
 		
-		return "artworkDetails";
+//		model.addAttribute("artwork", voto.getOpera());
+		model.addAttribute("artworks", this.operaService.getAllOpere());
+		return "artworks";
 	}
 	
 	@GetMapping("/editVoto/{id}")
@@ -63,16 +69,20 @@ public class VotoController {
 		return "editVoto";
 	}
 	
+	@Transactional
 	@PostMapping("/editVoto/{id}")
 	public String editingVoto(@PathVariable("id") Long id,
 								@ModelAttribute("voto") Voto voto, Model model) {
 		
 		Voto originalVoto = this.votoService.findVotoBtId(id);
 		originalVoto.setValutazione(voto.getValutazione());
+		originalVoto.setId(id);
 		this.votoService.save(originalVoto);
-		model.addAttribute("voto", originalVoto);
-		model.addAttribute("artwork", originalVoto.getOpera());
-		return "artworkDetails";
+//		model.addAttribute("voto", originalVoto);
+//		model.addAttribute("artwork", originalVoto.getOpera());
+//		return "artworkDetails";
+		model.addAttribute("artworks", this.operaService.getAllOpere());
+		return "artworks";
 	}
 	
 	@GetMapping("/removeVoto/{id}")
@@ -83,13 +93,14 @@ public class VotoController {
 		return "confirmDeletionVoto";
 	}
 	
+	@Transactional
 	@GetMapping("/deletionVoto/{id}")
 	public String removingVoto(@PathVariable("id") Long id, Model model) {
 		Voto voto = this.votoService.findVotoBtId(id);
-		model.addAttribute("artwork", voto.getOpera());
-		voto.getOpera().getVoti().remove(voto);
-		this.votoService.deleteVotoById(id);
-		model.addAttribute("voto", null);
-		return "artworkDetails";
+//		model.addAttribute("artwork", voto.getOpera());
+		this.votoService.deleteVotoById(id, voto);
+//		model.addAttribute("voto", null);
+		model.addAttribute("artworks", this.operaService.getAllOpere());
+		return "artworks";
 	}
 }
