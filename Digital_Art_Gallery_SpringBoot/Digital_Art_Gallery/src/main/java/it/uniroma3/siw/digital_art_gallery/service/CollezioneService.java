7543c1de823b909f55cc5d3696d0f305d480aacd
Davide Tedesco <davide_hrd@hotmail.it>
2022-06-26 01:56:48 +0200

    modified html, CSS and minor fixes

--- Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/service/CollezioneService.java
+++ Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/service/CollezioneService.java
@@ -13,37 +13,39 @@ import it.uniroma3.siw.digital_art_gallery.repository.CollezioneRepository;
 
 @Service
 public class CollezioneService {
-	
+
 	@Autowired
 	CollezioneRepository collezioneRepository;
-	
+
 	@Transactional
 	public Collezione save(Collezione collezione) {
+		if(collezione.getOpere()!=null) {
 		for(Opera o : collezione.getOpere()) {
 			o.setCollezione(collezione);
 		}
+		}
 		return this.collezioneRepository.save(collezione);
 	}
-	
-	public List<Collezione> getAllCollezioni(){
+
+	public List<Collezione> getAllCollezioni() {
 		return (List<Collezione>) collezioneRepository.findAll();
 	}
-	
+
 	@Transactional
 	public void deleteCollectionById(Long id) {
 		List<Opera> listaOpera = this.findCollezioneById(id).getOpere();
-		for(Opera o : listaOpera) {
+		for (Opera o : listaOpera) {
 			o.setCollezione(null);
 		}
 		this.collezioneRepository.deleteById(id);
 	}
-	
+
 	public Collezione findCollezioneById(Long id) {
 		return this.collezioneRepository.findById(id).get();
 	}
-	
+
 	public boolean verificaDuplicatiCollezione(String nome) {
 		return this.collezioneRepository.existsByNome(nome);
 	}
-	
+
 }
