500c456dea12c0d44e2017090d05753e1d91d339
Davide Tedesco <davide_hrd@hotmail.it>
2022-06-25 11:12:42 +0200

    corrected author removal and modified HTML and CSS

--- Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/controller/AutoreController.java
+++ Digital_Art_Gallery_SpringBoot/Digital_Art_Gallery/src/main/java/it/uniroma3/siw/digital_art_gallery/controller/AutoreController.java
@@ -20,111 +20,106 @@ import it.uniroma3.siw.digital_art_gallery.validator.AutoreValidator;
 
 @Controller
 public class AutoreController {
-	
+
 	@Autowired
 	AutoreService autoreService;
-	
+
 	@Autowired
 	AutoreValidator autoreValidator;
-	
+
 	@Autowired
 	LocalDateConverter converter;
 
 	@GetMapping("/authors")
 	public String autori(Model model) {
-		model.addAttribute("authors",autoreService.getAllAutori());
+		model.addAttribute("authors", autoreService.getAllAutori());
 		return "authors";
 	}
-	
+
 	@GetMapping("/admin/showContent/authors")
 	public String adminAutori(Model model) {
 		model.addAttribute("authors", this.autoreService.getAllAutori());
 		return "admin/showContentAuthors";
 	}
-	
+
 	@GetMapping("/admin/removeAuthor/{id}")
 	public String removeAutore(@PathVariable("id") Long id, Model model) {
 		List<Opera> listaOpere = this.autoreService.findAutoreById(id).getOpere();
-		//remove all authors artworks form all collection
-		for(Opera o : listaOpere) {
-			o.getCollezione().getOpere().remove(o);
+		// remove all authors artworks form all collection
+		for (Opera o : listaOpere) {
+			if (o.getCollezione() != null) {
+				o.getCollezione().getOpere().remove(o);
+			}
 		}
 		this.autoreService.deleteAuthorById(id);
 		model.addAttribute("authors", this.autoreService.getAllAutori());
 		return "admin/showContentAuthors";
 	}
-	
+
 	@GetMapping("/admin/confirmAuthorDeletion/{id}")
 	public String confirmDeletionAutore(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("author", this.autoreService.findAutoreById(id));
 		return "admin/askConfirmAuthorDeletion";
 	}
-	
+
 	@GetMapping("/admin/insertAuthor")
 	public String insertAuthor(Model model) {
 		model.addAttribute("author", new Autore());
 		return "admin/insertAuthor";
 	}
-	
+
 	@PostMapping("/admin/insertAuthor")
-	public String insertingAuthor(@ModelAttribute("author") Autore autore, 
-									@RequestParam("date") String date,Model model, 
-									BindingResult autoreBindingResults) {
-		
-		if(!autoreBindingResults.hasErrors()) {
+	public String insertingAuthor(@ModelAttribute("author") Autore autore, @RequestParam("date") String date,
+			Model model, BindingResult autoreBindingResults) {
+
+		if (!autoreBindingResults.hasErrors()) {
 			this.autoreService.save(autore, date);
 			model.addAttribute("authors", this.autoreService.getAllAutori());
 			return "admin/showContentAuthors";
 		}
-		
+
 		model.addAttribute("author", autore);
 		return "admin/insertAuthor";
 	}
-	
+
 	@GetMapping("/admin/editAuthor/{id}")
-	public String editAuthor(@PathVariable("id") Long id ,
-							Model model) {
+	public String editAuthor(@PathVariable("id") Long id, Model model) {
 		Autore autore = this.autoreService.findAutoreById(id);
 		model.addAttribute("data", this.converter.revertConversion(autore.getDataDiNascita()));
 		model.addAttribute("author", autore);
 		return "/admin/editAuthor";
 	}
-	
+
 	@PostMapping("/admin/editAuthor/{id}")
-	public String editingAuthor(@PathVariable("id") Long id,
-								@RequestParam("date") String date,
-								@ModelAttribute("author") Autore autore,
-								Model model,
-								BindingResult authorBindingResults) {
-		
+	public String editingAuthor(@PathVariable("id") Long id, @RequestParam("date") String date,
+			@ModelAttribute("author") Autore autore, Model model, BindingResult authorBindingResults) {
+
 		Autore autoreOriginale = this.autoreService.findAutoreById(id);
 		autoreOriginale.setNome(autore.getNome());
 		autoreOriginale.setCognome(autore.getCognome());
 		autoreOriginale.setLuogoDiNascita(autore.getLuogoDiNascita());
-		
-		//this.autoreValidator.validate(autoreOriginale, authorBindingResults);
-		
-		if(!authorBindingResults.hasErrors()) {
+
+		// this.autoreValidator.validate(autoreOriginale, authorBindingResults);
+
+		if (!authorBindingResults.hasErrors()) {
 			this.autoreService.save(autoreOriginale, date);
 			model.addAttribute("authors", autoreService.getAllAutori());
 			return "admin/showContentAuthors";
 		}
-		
+
 		model.addAttribute("author", autoreService.findAutoreById(id));
 		return "/admin/editAuthor";
 	}
-	
-	
-	
+
 	@GetMapping("/authorDetails/{id}")
 	public String dettagliAutore(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("author", this.autoreService.findAutoreById(id) );
+		model.addAttribute("author", this.autoreService.findAutoreById(id));
 		return "authorDetails";
 	}
-	
+
 	@GetMapping("/author/{id}/artworks")
 	public String operePerAutore(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("author", this.autoreService.findAutoreById(id) );
+		model.addAttribute("author", this.autoreService.findAutoreById(id));
 		model.addAttribute("artworks", this.autoreService.findAutoreById(id).getOpere());
 		return "artworksOfAuthor";
 	}
